ed5f0361b88ab61a7df9a25487a3bdaa5a7b0d3b
Alvin Nguyen <realalvinnerds@gmail.com>
2018-01-28 13:55:49 -0800

    Added Repair Game Assets

---
+++ BlueSky/Assets/Repair Mini Game Assets/Draggable.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	public Transform parentToReturnTo = null;
+
+    public enum Slot {LEFT, RIGHT, TOP, BOTTOM, OTHER};
+    public Slot typeOfItem = Slot.BOTTOM;
+	
+	public void OnBeginDrag(PointerEventData eventData) {
+		//Debug.Log ("OnBeginDrag");
+
+		parentToReturnTo = this.transform.parent;
+		this.transform.SetParent( this.transform.parent.parent );
+
+		GetComponent<CanvasGroup>().blocksRaycasts = false;
+	}
+
+	public void OnDrag(PointerEventData eventData) {
+		//Debug.Log ("OnDrag");
+
+		this.transform.position = eventData.position;
+	}
+	
+	public void OnEndDrag(PointerEventData eventData) {
+		//Debug.Log ("OnEndDrag");
+		this.transform.SetParent( parentToReturnTo );
+		GetComponent<CanvasGroup>().blocksRaycasts = true;
+	}
+	
+
+
+}
---
+++ BlueSky/Assets/Repair Mini Game Assets/DropZone.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
+
+    public Draggable.Slot typeOfItem = Draggable.Slot.BOTTOM;
+
+	public void OnPointerEnter(PointerEventData eventData) {
+		//Debug.Log("OnPointerEnter");
+	}
+	
+	public void OnPointerExit(PointerEventData eventData) {
+		//Debug.Log("OnPointerExit");
+	}
+	
+	public void OnDrop(PointerEventData eventData) {
+		//Debug.Log (eventData.pointerDrag.name + " was dropped on " + gameObject.name);
+
+		Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+		if(d != null && typeOfItem == d.typeOfItem) {
+			d.parentToReturnTo = this.transform;
+		}
+	}
+}
